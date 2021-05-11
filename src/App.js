32ed2385cba5237a6f1a9054b3fb47a2150ae777
Gerrit van Zyl <61610405+gerrit450@users.noreply.github.com>
2021-05-11 21:45:50 +1200

    modified CI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
-import React from 'react';
-import './App.css'; //importing css file
 import {Component} from 'react';
-import pic from  './assets/seeds.png';
 import run from './Form/form.js';
 
 class App extends Component {
