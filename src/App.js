be28d112b02c5e2cad085db74781f947b8d40d26
Gerrit van Zyl <61610405+gerrit450@users.noreply.github.com>
2021-05-11 21:15:30 +1200

    Added form francois created

--- src/App.js
+++ src/App.js
@@ -2,29 +2,16 @@ import React from 'react';
 import './App.css'; //importing css file
 import {Component} from 'react';
 import pic from  './assets/seeds.png';
+import run from './Form/form.js';
 
 class App extends Component {
 
   render()
   {
+  var r;
+  r = new run();
   return (
-    <div className="App">
-      <text style={{fontWeight:'bold',fontSize:32}}>Project SEEDS</text>
-      <img src={pic}  style={{resize: 'initial', width: 100}} alt={''}/>
-    <form action="http://localhost:4001/results" method="post">
-      <br/><br/>
-        <label htmlFor="header-search">
-            <span className="visually-hidden">Search for SE practices</span>
-            <br/>
-        </label>
-        <input
-            type="text"
-            placeholder="Search blog posts"
-            name="search" 
-        />
-        <button type="submit">Search</button>
-    </form>
-    </div>
+    r.create()
   );
   }
 
