159c452a619231d754819b902c558d33de396252
Gerrit van Zyl <61610405+gerrit450@users.noreply.github.com>
2021-05-20 20:03:44 +1200

    testing deploy to developseed

--- src/Form/form.js
+++ src/Form/form.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './style/style.css'; //importing css file
 import image1 from './images/SeedLogo.png'
 import { Component } from 'react';
+import Dropdown from 'react-bootstrap/Dropdown';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default class createForm extends Component
 {
   create()
   {
+      
   return (
+    
 <div>
 <head>
   <link rel="stylesheet" type="text/css" href="style/style.css"/>
@@ -22,14 +26,40 @@ export default class createForm extends Component
     <br/>
     Search for Software Engineering Articles
     <br/>
+    
     <form action="http://localhost:4001/results" method="post">
     <input type="text" name="search" placeholder="Search Articles"/> 
     <button type="submit" class="article">Search</button>
   </form>
+  <div style={{marginTop: 10}}>
+  <Dropdown className={"sepractice"}>
+  <Dropdown.Toggle variant="success" id="dropdown">
+   Select SE Practice
+  </Dropdown.Toggle>
+
+  <Dropdown.Menu>
+    <Dropdown.Item href="#/action-1">TDD</Dropdown.Item>
+  </Dropdown.Menu>
+</Dropdown>
+</div>
+<div style={{marginLeft:170, marginTop: - 38}}>
+<Dropdown className={"claim"}>
+  <Dropdown.Toggle variant="success" id="dropdown">
+   Choose a claim
+  </Dropdown.Toggle>
+
+  <Dropdown.Menu>
+    <Dropdown.Item href="#/action-1">Improve code quality</Dropdown.Item>
+    <Dropdown.Item href="#/action-1">Improve application quality</Dropdown.Item>
+    <Dropdown.Item href="#/action-1">Improve team confidence</Dropdown.Item>
+  </Dropdown.Menu>
+</Dropdown>
+</div>
   <div class="NewArticle">
     <button type="submit" >Add new articles</button>
   </div>
 </body>
+
 </div>
   );
   }
