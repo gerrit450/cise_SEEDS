389754ec0ce6e7e0394f6d1b4f8bcf81e9729aa7
Gerrit van Zyl <61610405+gerrit450@users.noreply.github.com>
2021-05-22 00:59:38 +1200

    try new deploy

--- src/Form/form.js
+++ src/Form/form.js
@@ -43,7 +43,7 @@ export default class createForm extends Component
     Search for Software Engineering Articles
     <br/>
     
-    <form action={PORT,'/results'} method="post">
+    <form action={PORT+'/results'} method="post">
     <input type="text" name="search" placeholder="Search Articles"/> 
     <button type="submit" class="article">Search</button>
   </form>
