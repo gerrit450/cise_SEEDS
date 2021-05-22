30146a89c3c1bf3d92edb7da3d95c536187d4eb6
Gerrit van Zyl <61610405+gerrit450@users.noreply.github.com>
2021-05-22 18:59:03 +1200

    try new deploy

--- src/connect.js
+++ src/connect.js
@@ -72,10 +72,9 @@ async function run() {
     .catch(error => console.error(error))
   })
 
-  app.use(express.static('testing'))
   // AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/testing/form.html'))
+  res.render('Test.ejs');
 })
 
 
